f0bf51f2c42763fdb273b61c802647d8718d3962
Sahil <sahilpandey412@gmail.com>
2025-03-04 10:55:45 -0600

    UC1

---
+++ HelloGreetingApp/BusinessLayer/Service/GreetingBL.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessLayer.Interface;
+using RepositoryLayer.Interface;
+
+namespace BusinessLayer.Service
+{
+    public class GreetingBL : IGreetingBL
+    {
+        
+    }
+}
