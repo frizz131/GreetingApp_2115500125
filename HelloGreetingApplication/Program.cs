1372c1863a2a6fbb7ea31695bf9b0339fdbc50cf
Sahil <sahilpandey412@gmail.com>
2025-03-04 23:52:42 -0600

    UC1

---
+++ HelloGreetingApplication/Program.cs
@@ -0,0 +1,31 @@
+using BusinessLayer.Interface;
+using BusinessLayer.Service;
+using RepositoryLayer.Interface;
+using RepositoryLayer.Service;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+//Add Swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ RepositoryLayer/Service/GreetingRL.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RepositoryLayer.Interface;
+
+namespace RepositoryLayer.Service
+{
+   
+}
